8396c28614d1f3c5c5d7623f81bbaa485439ae3d
Pierre Ablin <22317488+pierreablin@users.noreply.github.com>
2022-07-04 21:59:43 +0200

    ENH: add a noise parameter in the simulated data (#4)
    * add noise parameter
    
    * fix flake
    
    * revert obj
    
    * simplify
    
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -10,18 +10,25 @@ class Dataset(BaseDataset):
     name = "Simulated"
 
     parameters = {
-        'n_matrices, n_features': [
-            (100, 5)],  # slow to simulate big correlated design
+        'n_matrices, n_features, noise_level': [
+            (100, 5, 0.01), (100, 5, 0.1)]
     }
 
-    def __init__(self, n_matrices=10, n_features=5, random_state=27):
+    def __init__(self, n_matrices=10, n_features=5, noise_level=0,
+                 random_state=27):
         self.n_matrices = n_matrices
         self.n_features = n_features
+        self.noise_level = noise_level
         self.random_state = random_state
 
     def get_data(self):
         rng = np.random.RandomState(self.random_state)
         diagonals = rng.uniform(size=(self.n_matrices, self.n_features))
         A = rng.randn(self.n_features, self.n_features)  # mixing matrix
-        C = (A[None, :, :] * diagonals[:, None, :]) @ A.T  # dataset
+        C = (A[None, :, :] * diagonals[:, None, :]) @ A.T  # clean dataset
+        N = self.noise_level * rng.randn(self.n_matrices, self.n_features,
+                                         self.n_features)
+        # create random psd matrices and add them to C
+        N = N @ np.transpose(N, axes=(0, 2, 1))
+        C += N
         return dict(C=C, A=A)
